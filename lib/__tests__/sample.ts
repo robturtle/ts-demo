196d7c2f9f1d326546ac76ec455603b2ce875ac7
Yang Liu <jeremyrobturtle@gmail.com>
2019-12-23 18:39:16 -0800

    test lint-staged for ts

--- lib/__tests__/sample.ts
+++ lib/__tests__/sample.ts
@@ -1,3 +1,13 @@
+const aVeryLooooooooooongName = {
+  looooooooooooooooooooooooooooooooooooooooooooooooong() {
+    return this;
+  }
+};
+
 test('1 + 1 === 2', () => {
-	expect(1 + 1).toBe(2);
+  expect(1 + 1).toBe(2);
+  /* eslint max-len: ["error", { "code": 80 }] */
+  aVeryLooooooooooongName
+    .looooooooooooooooooooooooooooooooooooooooooooooooong()
+    .looooooooooooooooooooooooooooooooooooooooooooooooong();
 });

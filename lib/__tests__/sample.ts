4aed595a10578d2b589affc4b99fcaa2e82bcfd4
Yang Liu <jeremyrobturtle@gmail.com>
2019-12-23 18:42:55 -0800

    tesst fuck me

--- lib/__tests__/sample.ts
+++ lib/__tests__/sample.ts
@@ -1,10 +1,10 @@
 const aVeryLooooooooooongName = {
   looooooooooooooooooooooooooooooooooooooooooooooooong() {
     return this;
-  }
+  },
 };
 
-test("1 + 1 === 2", () => {
+test('1 + 1 === 2', () => {
   expect(1 + 1).toBe(2);
   /* eslint max-len: ["error", { "code": 80 }] */
   aVeryLooooooooooongName

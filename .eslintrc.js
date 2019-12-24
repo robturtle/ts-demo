d2ab321d6cd1820dedd13f63101e0a764d4ece03
Yang Liu <jeremyrobturtle@gmail.com>
2019-12-23 19:15:09 -0800

    cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,12 @@ module.exports = {
     jest: true,
     node: true,
   },
-  extends: ['prettier', 'airbnb', 'plugin:@typescript-eslint/recommended'],
+  extends: ['prettier', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
   settings: {
     'import/parsers': {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-    'import/resolver': {
-      typescript: {},
     },
   },
   rules: {
--- lib/__tests__/sample.ts
+++ lib/__tests__/sample.ts
@@ -1,3 +1,5 @@
+import { plus } from '..';
+
 test('1 + 1 === 2', () => {
-  expect(1 + 1).toBe(2);
+  expect(plus(1, 1)).toBe(2);
 });
